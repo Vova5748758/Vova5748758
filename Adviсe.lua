b12c494ac12ba21230fb5919292ec4b941aa5b0d
Vova5748758 <vovansm334@gmail.com>
2024-12-30 23:03:07 +0300

    Update Adviсe.lua

--- Adviсe.lua
+++ Adviсe.lua
@@ -10,6 +10,24 @@ loadstring(game:HttpGet("ваша сыллка"))()
 подробное на https://youtu.be/d8mJAo8KXyE?si=sVjBjOiUHN3Xd2RH (НЕ АВТОР)
 
 пишите вопросы на hffh65910@gmail.com
+скрипт GUI
+-----------
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/turtle"))()
+local Window = library:Window("Window") --you can copy and install it so that there are two floating window
+Window:Button("Button", function() --Button 
+print("click")
+end)
+Window:Box("TextBox", function(text, focuslost)--TextBox
+        print("TextBox")
+end)
+
+Window:Text("Text", function(text, focuslost)
+    text = "Text"
+end)
+
+--скрипт от CDTY0898 
+-----------
+
 
 **ENGLISH 
 I recommend using pastebin, it's pretty easy to useI recommend using pastebin, it's pretty easy to use
@@ -24,3 +42,21 @@ loadstring(game:HttpGet("your link"))()
 more details at https://youtu.be/d8mJAo8KXyE?si=sVjBjOiUHN3Xd2RH (NOT THE AUTHOR!!!!)
 
 details on hffh65910@gmail.com
+script GUI
+
+-----------
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/turtle"))()
+local Window = library:Window("Window") --you can copy and install it so that there are two floating window
+Window:Button("Button", function() --Button 
+print("click")
+end)
+Window:Box("TextBox", function(text, focuslost)--TextBox
+        print("TextBox")
+end)
+
+Window:Text("Text", function(text, focuslost)
+    text = "Text"
+end)
+
+--script from CDTY0898 
+-----------
